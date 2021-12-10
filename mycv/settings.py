123caacdd28f7df864ba1408a3d2dad5409107c6
aminfranko <aminfranko@gmail.com>
2021-12-10 18:25:46 +0330

    End point
    ready to push to heroku

--- mycv/settings.py
+++ mycv/settings.py
@@ -132,3 +132,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku Settings
+import django_heroku
+django_heroku.settings(locals())
+
